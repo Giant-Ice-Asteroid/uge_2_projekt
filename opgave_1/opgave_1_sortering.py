c7e4969fbbd3b1604cf40332a002ecce66be028b
Giant-Ice-Asteroid <amalie.heurlin@specialisterne.com>
2025-03-04 15:59:21 +0100

    Arbejdet videre på opgave 1(ikke helt færdig/streamlinet og godt igang med opgave 2)

--- opgave_1/opgave_1_sortering.py
+++ opgave_1/opgave_1_sortering.py
@@ -20,18 +20,6 @@ name_data_list_alpha = sorted(name_data_list)
 name_data_list_len = sorted(name_data_list, key=len)
 #print(name_data_list_len)
 
-# optælling af bogstaver i navn
-
-from collections import defaultdict
-
-name_data_dict = defaultdict(list)
-
-for name in name_data_list:
-    count = len(name)
-    name_data_dict[name].append(count)
-
-        
-# print(name_data_dict)
 
 #optælling af enkelte bogstaver
 
@@ -51,4 +39,84 @@ for name in name_data_list:
 
     
           
-print(letter_count)
+#print(letter_count)
+#### work in progress
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud
+import numpy as np
+from PIL import Image
+
+keys = list(letter_count.keys())
+values = list(letter_count.values())
+
+# Frekvensanalyse
+
+# sns.barplot(data = letter_count, x=keys, y = values)
+# plt.show()
+
+# Wordcloud
+wc = WordCloud(background_color="white",width=1000,height=1000).generate_from_frequencies(letter_count)
+plt.figure(figsize=(15,8))
+#plt.imshow(wc)
+#plt.show()
+
+
+# NAVNELÆNGDE ANALYSE
+
+# optælling af antal bogstaver i navne
+
+from collections import defaultdict
+
+name_data_dict = defaultdict(list)
+
+for name in name_data_list:
+    count = len(name)
+    name_data_dict[name].append(count)
+
+        
+# print(name_data_dict)
+
+# frekvensen af navne af en given længde..
+dict(name_data_dict)
+
+freq_dict = defaultdict(int)
+
+for key, val in name_data_dict.items():
+    for count in val:
+        freq_dict[count] += 1
+
+print(freq_dict)
+
+len_key = list(freq_dict.keys())
+len_val = list(freq_dict.values())
+sns.barplot(data = freq_dict, x = len_key, y = len_val)
+#plt.show()
+
+# median navnelængde
+
+alle_values = []
+for key, value in freq_dict.items():
+    alle_values.extend([key] * value)
+
+median = np.median(alle_values)
+print(f"Medianen er = {median}")
+      
+# mean navnelængde
+
+total_count = sum(freq_dict.values())
+
+mean = sum(key * value for key, value in freq_dict.items()) / total_count
+
+print(f"Gennemsnittet er = {mean}")
+
+"""
+plt.figure(figsize=(10, 8))
+sns.boxplot(x=len_key,
+            y=len_val,
+            data=freq_dict,
+            showmeans=True)  # notice the change
+plt.ylabel("Antal_personer", size=14)
+plt.xlabel("Antal_bogstaver", size=14)
+plt.show()"""
---
+++ opgave_2/opgave_2.py
@@ -0,0 +1,39 @@
+"""
+Opgave 2: Logfil analyse 
+
+Opret et script, der læser indholdet fra en logfil (app_log.txt) og filtrerer bestemte typer 
+af logmeddelelser, som ERROR og WARNING. 
+Gem de filtrerede logmeddelelser i en ny fil, så kritiske hændelser er lette at overskue 
+
+"""
+
+import os
+import re
+
+log_file = "opgave_2\\app_log.txt"
+
+warning_count = 0  # optælling  af "WARNING" forekomster
+error_count = 0  # optælling af "ERROR" forekomster
+warning_lines = []  # Linjer i tekstfilen hvor ordet "WARNING" forekommer
+error_lines = []  # Linjer i tekstfilen hvor ordet "ERROR" forekommer
+
+with open(log_file, "r") as file:
+    for line_number, line in enumerate(file, 1):  # Starter line_number from 1 f
+        warning_matches = re.findall(r"WARNING", line)  # Finder "WARNING" på pågældende linje
+        error_matches = re.findall(r"ERROR", line)  # ditto "ERROR" 
+        
+        if warning_matches:  #hvis  "WARNING" optræder på linje
+            warning_count += len(warning_matches)  # inkrementerer count
+            warning_lines.append(line_number)  # tilføjer linje nummer til warning liste
+        
+        if error_matches:  
+            error_count += len(error_matches)  
+            error_lines.append(line_number)  
+
+# resultat
+print(f"Antal forekomster af 'WARNING': {warning_count}")
+print(f"Linjer hvor 'WARNING' forefindes: {warning_lines}")
+print(f"Antal forekomster af 'ERROR': {error_count}")
+print(f"Linjer hvor 'ERROR' forefindes: {error_lines}")
+
+
